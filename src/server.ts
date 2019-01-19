16a47f67cacb08600cdca1923430790c01ee08e5
Max Watkins <willmwatkins@gmail.com>
2019-01-19 17:08:02 +1100

    new path for diagnosing

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 /* app/server.ts */
 // Import everything from express and assign it to the express variable
-import express from 'express';
+import express, { Router } from 'express';
+import { Request, Response } from 'express';
+
 import bodyParser from 'body-parser';
 // Import WelcomeController from controllers entry point
 import {DefaultController} from './controllers';
@@ -10,10 +12,19 @@ const app: express.Application = express();
 // The port the express app will listen on
 const port= process.env.PORT || 8080;
 
+console.log('setting up on port ' + port);
+
 const urlEndodedParser = bodyParser.urlencoded({extended: false});
 // Mount the WelcomeController at the /welcome route etc
 
 app.use(express.static(join(__dirname,'public')));
+
+let router: Router = Router();
+router.use((req: Request, res: Response) => {
+    res.send('hi back');
+});
+
+app.use('/hi', router);
 app.use('*', DefaultController);
 // Serve the application at the given port
 app.listen(port, () => {
