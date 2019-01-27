fd2d9e8e93e9256383c14078776bf2c4c2da65ff
Max Watkins <willmwatkins@gmail.com>
2019-01-27 22:28:58 +1100

    404 -> 200

--- src/Controllers/defaultController.ts
+++ src/Controllers/defaultController.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 
 const router: Router = Router();
 router.use('/', (req: Request, res: Response) => {
-      res.send('404 not found');
+      res.send(200);
 });
 
 export const DefaultController: Router = router;
