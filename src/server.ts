aaa038dc2fb10e687db28e47986f91354847ed45
Max Watkins <willmwatkins@gmail.com>
2019-01-27 22:23:26 +1100

    just the 404?

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,19 @@ import {DefaultController} from './controllers';
 // Create a new express application instance
 const app: express.Application = express();
 // The port the express app will listen on
+<<<<<<< HEAD
 const port= process.env.PORT || 8080;
 
 // Mount the WelcomeController at the /welcome route etc
 app.use('/', express.static('public'));
 
 app.use('*', DefaultController);
+=======
+const port: number = 80;
+// Mount the WelcomeController at the /welcome route
+app.use('/welcome', WelcomeController);
+app.use('/', SplashController);
+>>>>>>> parent of a6c2432... fixed to work on Azure
 // Serve the application at the given port
 app.listen(port, () => {
 // Success callback
