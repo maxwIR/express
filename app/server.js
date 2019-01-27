8864e50468f662afed377011bf1352f1397556e8
Max Watkins <willmwatkins@gmail.com>
2019-01-27 18:29:21 +1100

    cleanup and reran build

--- app/server.js
+++ app/server.js
@@ -1,19 +1,11 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 /* app/server.ts */
 // Import everything from express and assign it to the express variable
-const express_1 = __importStar(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
+const express_1 = __importDefault(require("express"));
 // Import WelcomeController from controllers entry point
 const controllers_1 = require("./controllers");
 const path_1 = require("path");
@@ -21,15 +13,8 @@ const path_1 = require("path");
 const app = express_1.default();
 // The port the express app will listen on
 const port = process.env.PORT || 8080;
-console.log('setting up on port ' + port);
-const urlEndodedParser = body_parser_1.default.urlencoded({ extended: false });
 // Mount the WelcomeController at the /welcome route etc
-app.use(express_1.default.static(path_1.join(__dirname, 'public')));
-let router = express_1.Router();
-router.use((req, res) => {
-    res.send('hi back');
-});
-app.use('/hi', router);
+app.use("home", express_1.default.static(path_1.join(__dirname, 'public')));
 app.use('*', controllers_1.DefaultController);
 // Serve the application at the given port
 app.listen(port, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 /* app/server.ts */
 // Import everything from express and assign it to the express variable
-import express, { Router } from 'express';
+import express from 'express';
 import { Request, Response } from 'express';
 
 import bodyParser from 'body-parser';
@@ -12,14 +12,8 @@ const app: express.Application = express();
 // The port the express app will listen on
 const port= process.env.PORT || 8080;
 
-console.log('setting up on port ' + port);
-
-const urlEndodedParser = bodyParser.urlencoded({extended: false});
 // Mount the WelcomeController at the /welcome route etc
-
 app.use("home", express.static(join(__dirname,'public')));
-
-let router: Router = Router();
 
 app.use('*', DefaultController);
 // Serve the application at the given port
